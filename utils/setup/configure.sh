2eec9358505f02fbb2bc32b32b52a34300653c6b
Raul Schmidlin <raul.schmidlin@logictiles.com>
2017-12-24 09:42:30 +0100

    Adapting minsoc initialization to Git times.

--- utils/setup/configure.sh
+++ utils/setup/configure.sh
@@ -1,5 +1,12 @@
 . ${SCRIPT_DIR}/beautify.sh
 
+#Clone missing repositories
+cecho "\nImporting required repositories"
+execcmd "Clone UART16550 from Github FreeCores" "git clone https://github.com/freecores/uart16550.git ${DIR_TO_INSTALL}/minsoc/rtl/verilog/uart16550"
+execcmd "Clone Advanced Debug System from Github FreeCores" "git clone https://github.com/freecores/adv_debug_sys.git ${DIR_TO_INSTALL}/minsoc/rtl/verilog/adv_debug_sys"
+execcmd "Clone Ethernet MAC from Github FreeCores" "git clone https://github.com/freecores/ethmac.git ${DIR_TO_INSTALL}/minsoc/rtl/verilog/ethmac"
+execcmd "Checkout OR1200 from Opencores" "svn co http://opencores.org/ocsvn/openrisc/openrisc/trunk/or1200 ${DIR_TO_INSTALL}/minsoc/rtl/verilog/or1200"
+
 #Configuring MinSoC
 cecho "\nConfiguring MinSoC"
 execcmd "cd ${DIR_TO_INSTALL}/minsoc/backend/std"
